a439ec3d4bdf8e7ca29dc227ffa94d7c8db05678
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-07-30 16:58:10 +0400

    feat: smear

--- init.lua
+++ init.lua
@@ -266,6 +266,19 @@ require('lazy').setup({
   --            })
   --        end,
   --    }
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = { -- Default  Range
+      time_interval = 7, -- milliseconds
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+      trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+      damping = 0.8, -- 0.65     [0, 1]
+      damping_insert_mode = 0.8, -- 0.7      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+  },
   --
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
