afdb25e8edb2b25d6ab7a4bacdf0d9c3fc406aeb
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-08-30 14:49:07 +0400

    switch mini-surround to vim-surround
    mainly changing due to muscle memory of vim-surround in vscode
    
    this might be changed back later on if i find mini.nvim surround to be better

--- init.lua
+++ init.lua
@@ -921,23 +921,8 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
@@ -956,6 +941,9 @@ require('lazy').setup({
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
+  },
+  {
+    'tpope/vim-surround',
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
