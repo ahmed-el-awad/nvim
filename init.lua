e08a4df9ff0257293dc1f8761352c7af7d4ef19e
Ahmed Elawad <73728149+ahmed-el-awad@users.noreply.github.com>
2026-01-18 03:36:27 +0400

    split size control

--- init.lua
+++ init.lua
@@ -65,6 +65,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+--  Change the size of the splits
+vim.keymap.set('n', '<M-h>', '<cmd>vertical resize +5<CR>')
+vim.keymap.set('n', '<M-l>', '<cmd>vertical resize -5<CR>')
+vim.keymap.set('n', '<M-j>', '<cmd>resize +5<CR>')
+vim.keymap.set('n', '<M-k>', '<cmd>resize -5<CR>')
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
